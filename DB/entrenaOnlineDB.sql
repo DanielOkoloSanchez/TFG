c4fb338a06530ec6b9bb5bcbac87c90871dc54ba
daniel okolo <dokolosfp1@gmail.com>
2023-04-16 23:37:41 +0200

    primera version del proyecto

---
+++ DB/entrenaOnlineDB.sql
@@ -0,0 +1,82 @@
+drop database if exists entrenaOnlineDB ;
+create database entrenaOnlineDB;
+Use entrenaOnlineDB;
+
+CREATE TABLE usuario (
+  id INT PRIMARY KEY,
+  nombre VARCHAR(25) NOT NULL,
+  clave VARCHAR(255) NOT NULL,
+  rango ENUM('admin', 'client') NOT NULL
+);
+
+CREATE TABLE cliente (
+  id INT PRIMARY KEY,
+  nombre VARCHAR(25) NOT NULL,
+  primerApellido VARCHAR(25) NOT NULL,
+  segundoApellido VARCHAR(25) NOT NULL,
+  edad INT NOT NULL,
+  altura DECIMAL(3,2) NOT NULL,
+  peso DECIMAL(4,1) NOT NULL,
+  complexion ENUM('Hectomorfo', 'Mesoformo', 'Endomorfo') NOT NULL,
+  objetivo ENUM('mantenimiento', 'volumen', 'definicion') NOT NULL
+);
+
+CREATE TABLE comidas (
+  id INT PRIMARY KEY,
+  nombre VARCHAR(25) NOT NULL,
+  descripcion VARCHAR(255) NOT NULL,
+  tipo ENUM('mantenimiento', 'volumen', 'definicion') NOT NULL
+);
+
+CREATE TABLE entrenamientos (
+  id INT PRIMARY KEY,
+  nombre VARCHAR(25) NOT NULL,
+  descripcion VARCHAR(255) NOT NULL,
+  parteCuerpo ENUM ("pierna","brazo","pecho","espalda") NOT NULL,
+  objetivo ENUM('mantenimiento', 'volumen', 'definicion') NOT NULL
+);
+
+CREATE TABLE rutinas (
+  id INT PRIMARY KEY,
+  nombreRutina varchar(30) NOT NULL, 
+  diaUno VARCHAR(255) NOT NULL,
+  diaDos VARCHAR(255) NOT NULL,
+  diaTres VARCHAR(255) NOT NULL,
+  diaCuatro VARCHAR(255) NOT NULL,
+  diaCinco VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE planesAlimentarios (
+  id INT PRIMARY KEY,
+  nombrePlan varchar(30) NOT NULL,
+  Lunes VARCHAR(255),
+  Martes VARCHAR(255),
+  Miercoles VARCHAR(255),
+  Jueves VARCHAR(255),
+  Viernes VARCHAR(255),
+  Sabado VARCHAR(255),
+  Domingo VARCHAR(255)
+);
+
+CREATE TABLE cliente_rutina (
+  id INT PRIMARY KEY,
+  idCliente INT NOT NULL,
+  idRutina INT NOT NULL,
+  FOREIGN KEY (idCliente) REFERENCES cliente(id),
+  FOREIGN KEY (idRutina) REFERENCES rutinas(id)
+);
+
+CREATE TABLE cliente_planesAlimentarios (
+  id INT PRIMARY KEY,
+  idCliente INT NOT NULL,
+  idPlanAlimentario INT NOT NULL,
+  FOREIGN KEY (idCliente) REFERENCES cliente(id),
+  FOREIGN KEY (idPlanAlimentario) REFERENCES planesAlimentarios(id)
+);
+
+CREATE TABLE anuncios (
+  id INT PRIMARY KEY,
+  nombre VARCHAR(255) NOT NULL,
+  descripcion TEXT NOT NULL,
+  fecha DATE NOT NULL
+);
---
+++ Vistas/html/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="css/login.css">
+</head>
+
+<body>
+    
+    <div class="login-form">
+        <h1>Iniciar Sesion</h1>
+        <form>
+           
+            <input type="text" placeholder="Usuario">
+            <input type="password" placeholder="Contraseña">
+            <input type="submit" value="Iniciar sesión">
+        </form>
+        <!-- <a href="#">¿Olvidaste tu contraseña?</a> -->
+    </div>
+</body>
+
+</html>
